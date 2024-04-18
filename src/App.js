73431be32c6eb8c102ae0ac327c5407a0359dd51
aref <arefhukan@gmail.com>
2024-04-18 12:31:17 -0700

    Restaurant-LandingPage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Testimonial from "./components/Testimonial";
+import Work from "./components/Work";
+import Home from "./components/home";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home />
+      <About />
+      <Work />
+      <Testimonial />
+      <Contact />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Work.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import PickMeals from "../Assest/pick-meals-image.png";
+import ChoseMeals from "../Assest/choose-image.png";
+import DeliveryMeals from "../Assest/delivery-image.png";
+
+const Work = () => {
+  const workInfoData = [
+    {
+      pic: PickMeals,
+      title: "Pick Meals",
+      text: "Lorem ipsum dolor sit amet consectetur. Maecenas orci et sagittis duis elementum interdum facilisi bibendum.",
+    },
+    {
+      pic: ChoseMeals,
+      title: "Choose How Often",
+      text: "Lorem ipsum dolor sit amet consectetur. Maecenas orci et ",
+    },
+    {
+      pic: DeliveryMeals,
+      title: "Fast Deliveries",
+      text: "Lorem ipsum dolor sit amet consectetur. Maecenas orci et lorem ipsum",
+    },
+  ];
+  return (
+    <div className="work-section-wrapper">
+      <div className="work-section-top">
+        <p className="primary-subheading">Work</p>
+        <h1 className="primary-heading">How It Work</h1>
+        <p className="primary-text">
+          Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
+          elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.
+        </p>
+      </div>
+
+      <div className="work-section-bottom">
+        {workInfoData.map((data) => (
+          <div className="work-section-info">
+            <div className="info-boxes-img-container">
+              <img src={data.pic} alt="" />
+            </div>
+            <h2>{data.title}</h2>
+            <p>{data.text}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Work;
